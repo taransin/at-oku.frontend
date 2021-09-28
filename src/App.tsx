00a7f6e8fc8a98f55f76c4576bb2cf83696f9919
Davide Ciacco <davide.ciacco@neosperience.com>
2021-09-28 23:02:25 +0200

    nevermind

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Home, VideoChat } from './containers';
 import { useSelector } from 'react-redux';
 import { RootState } from './store/store';
-// import { createTheme, ThemeProvider } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material';
 
 const SafeRoute = (props: any) => {
   const username = useSelector(
@@ -14,51 +14,51 @@ const SafeRoute = (props: any) => {
 };
 
 function App() {
-  // const customTheme = createTheme({
-  //   palette: {
-  //     mode: 'dark',
-  //     primary: {
-  //       light: '#afa',
-  //       main: '#99e5df',
-  //       dark: '#afa',
-  //       contrastText: '#fff',
-  //     },
-  //     secondary: {
-  //       light: '#afa',
-  //       main: '#f11',
-  //       dark: '#faf',
-  //       contrastText: '#faf',
-  //     },
-  //   },
-  //   components: {
-  //     // Name of the component
-  //     MuiButton: {
-  //       styleOverrides: {
-  //         // Name of the slot
-  //         root: {
-  //           // Some CSS
-  //           fontSize: '1rem',
-  //           backgroundColor: '#357f78',
-  //           ':hover': {
-  //             backgroundColor: '#1a746b',
-  //           },
-  //           color: 'white',
-  //           outlineColor: 'white',
-  //         },
-  //       },
-  //     },
-  //   },
-  // });
+  const customTheme = createTheme({
+    palette: {
+      mode: 'dark',
+      primary: {
+        light: '#afa',
+        main: '#99e5df',
+        dark: '#afa',
+        contrastText: '#fff',
+      },
+      secondary: {
+        light: '#afa',
+        main: '#f11',
+        dark: '#faf',
+        contrastText: '#faf',
+      },
+    },
+    components: {
+      // Name of the component
+      MuiButton: {
+        styleOverrides: {
+          // Name of the slot
+          root: {
+            // Some CSS
+            fontSize: '1rem',
+            backgroundColor: '#357f78',
+            ':hover': {
+              backgroundColor: '#1a746b',
+            },
+            color: 'white',
+            outlineColor: 'white',
+          },
+        },
+      },
+    },
+  });
 
   return (
-    // <ThemeProvider theme={customTheme}>
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <SafeRoute path="/video" component={VideoChat} />
-      </Switch>
-    </BrowserRouter>
-    // </ThemeProvider>
+    <ThemeProvider theme={customTheme}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <SafeRoute path="/video" component={VideoChat} />
+        </Switch>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
